8fab34e8cf45c357e392df7a51bcfbee2e24918f
dly <devninja17@users.noreply.github.com>
2017-10-04 23:41:11 -0700

    Functioning slider

--- Slider and Multiplication Table/ViewController.swift
+++ Slider and Multiplication Table/ViewController.swift
@@ -8,8 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        tableView.reloadData()
+        print(slider.value)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = String(Int(slider.value * 20) * (indexPath.row + 1))
+        return cell
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
